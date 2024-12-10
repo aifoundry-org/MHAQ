d468c4f88bcd75a41ef0936107279d3d55b3e453
janchk <janchk@gmail.com>
2024-12-10 22:22:39 +0000

    Bestowed a new model checkpoint, 'resnet20_new', upon the neural network's domain.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from torchvision.models import resnet18
 from .resnet.resnet_cifar import resnet20_cifar10
+from .resnet.resnet_cifar import resnet20_cifar10_new
 from .resnet.resnet_cifar import resnet20_cifar100
 
-__all__ = ["resnet18", "resnet20_cifar10", "resnet20_cifar100"]
+__all__ = ["resnet18", "resnet20_cifar10", "resnet20_cifar100", "resnet20_cifar10_new"]
