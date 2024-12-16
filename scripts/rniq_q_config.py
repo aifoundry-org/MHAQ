d263d1d54033e71020fcf3e7bc51cad49eba3b9e
janchk <janchk@gmail.com>
2024-12-16 22:26:19 +0000

    Arguments. That should've been done a while ago.

--- scripts/rniq_q_config.py
+++ scripts/rniq_q_config.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import torch
+import argparse
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
@@ -12,23 +13,38 @@ from src.training.trainer import Trainer
 
 torch.set_float32_matmul_precision('high')
 
-# config = load_and_validate_config("config/rniq_config_resnet20_cifar100.yaml")
-config = load_and_validate_config("config/rniq_config_resnet20_old.yaml")
-# config = load_and_validate_config("config/rniq_config_resnet20_new.yaml")
-dataset_composer = DatasetComposer(config=config)
-model_composer = ModelComposer(config=config)
-quantizer = Quantizer(config=config)()
-trainer = Trainer(config=config)
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run RNIQ quantization.")
+    parser.add_argument(
+        "--config", 
+        type=str, 
+        required=False, 
+        help="Path to the configuration file (YAML).",
+        default="config/rniq_config_resnet20_old.yaml"
+    )
 
-data = dataset_composer.compose()
-model = model_composer.compose()
-qmodel = quantizer.quantize(model, in_place=True)
 
-# Test model before quantization
-trainer.test(model, datamodule=data)
+def main():
+    args = parse_args()
 
-# Finetune model
-trainer.fit(qmodel, datamodule=data)
+    config = load_and_validate_config(args.config)
+    dataset_composer = DatasetComposer(config=config)
+    model_composer = ModelComposer(config=config)
+    quantizer = Quantizer(config=config)()
+    trainer = Trainer(config=config)
 
-# Test model after quantization
-trainer.test(qmodel, datamodule=data)
+    data = dataset_composer.compose()
+    model = model_composer.compose()
+    qmodel = quantizer.quantize(model, in_place=True)
+
+    # Test model before quantization
+    trainer.test(model, datamodule=data)
+
+    # Finetune model
+    trainer.fit(qmodel, datamodule=data)
+
+    # Test model after quantization
+    trainer.test(qmodel, datamodule=data)
+
+if __name__ == "__main__":
+    main()
