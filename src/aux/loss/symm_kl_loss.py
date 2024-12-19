3bc3bf29fe248a9adc1e7b22fecb47997b938abb
Sergey Salishev <sergey.i.salishev@gmail.com>
2024-12-19 14:09:56 +0300

    new losses and loss fixes

---
+++ src/aux/loss/symm_kl_loss.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn.modules.loss import _Loss, _Reduction
+
+class SymmetricalKL(_Loss):
+    def __init__(self, size_average=None, reduce=None, reduction: str = 'mean') -> None:
+        super().__init__(size_average, reduce, reduction)
+            
+    
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        x = F.log_softmax(input, dim=1)
+        y = F.log_softmax(target, dim=1)
+        return F.kl_div(x, y, log_target=True) + F.kl_div(y, x, log_target=True)
