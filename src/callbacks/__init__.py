daf7cd69958d7ff17b69f6988bf59346fd16a185
Jan Akhremchik <janchk@gmail.com>
2024-12-21 02:29:28 +0400

    Dev/early stopping (#40)
    * Init early stopping implementation
    
    * decent config

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -2,5 +2,13 @@ from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 from .lr_loss_revert import ReduceLrOnOutlier
 from .noise_ratio_adjust import RandNoiseScale
 from .violin_vis import DistillViolinVis
+from .early_stopping import NoiseEarlyStopping
 
-__all__ = ["ModelCheckpoint", "EarlyStopping", "ReduceLrOnOutlier", "RandNoiseScale", "DistillViolinVis"]
+__all__ = [
+    "ModelCheckpoint",
+    "EarlyStopping",
+    "ReduceLrOnOutlier",
+    "RandNoiseScale",
+    "DistillViolinVis",
+    "NoiseEarlyStopping",
+]
