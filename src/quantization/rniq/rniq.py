29dba7704b944b024ce8600cae11eab9a28867e6
Sergey Salishev <sergey.i.salishev@gmail.com>
2024-12-29 15:05:27 +0300

    fix logic, add asserts

--- src/quantization/rniq/rniq.py
+++ src/quantization/rniq/rniq.py
@@ -1,12 +1,13 @@
 import torch
 
 from torch import Tensor
-from src.quantization.rniq.utils.enums import QMode
+from torch.nn.modules import Module 
 
 
 class Quantizer:
     def __init__(
         self,
+        module: torch.nn.modules.Module,
         scale: torch.Tensor,
         zero_point: torch.Tensor,
         min_val: torch.Tensor,
@@ -23,6 +24,7 @@ class Quantizer:
             max_val (float): _description_
             rnoise_ratio (float): _description_
         """
+        self.module = module
         self.scale = scale
         self.zero_point = zero_point  # zero point
         self.min_val = min_val
@@ -51,14 +53,20 @@ class Quantizer:
 
         zero_noise = torch.zeros_like(value)
 
-        clamped_value = (
-            # torch.clamp(value / self.scale, min=self.min_val, max=self.max_val) - self.zero_point
-            torch.clamp(value, min=self.min_val, max=self.max_val)
-        )
+        # clamp is used only for activations
+        # the clamp is before noise beacause adding rounding noise is equivalent to rounding clamp
+        value = torch.clamp(value, min=self.min_val, max=self.max_val)
+
+        value = value - self.zero_point
+
+        if self._is_positive_scale():
+            value = value / self.scale
+
 
         if self.rnoise_ratio.item() == -1.0 or not self._is_positive_scale():
             # No need to calculate noise at all
-            return clamped_value
+            rnoise = zero_noise
+            qnoise = zero_noise
         elif self.rnoise_ratio.item() == 0.0:
             # Disable random noise calculation
             rnoise = zero_noise
@@ -71,14 +79,20 @@ class Quantizer:
             qnoise = self._get_qnoise(value)
             rnoise = self._get_rnoise(value)
 
-        noise = self.scale * (
-            self.rnoise_ratio * rnoise + (1 - self.rnoise_ratio) * qnoise)
+        noise = self.rnoise_ratio * rnoise + (1 - self.rnoise_ratio) * qnoise
         
+        value = value + noise.detach()
 
-        # if self._is_positive_scale():
-        # return torch.floor(clamped_value / self.scale + 0.5)
-
-        return clamped_value / self.scale + noise.detach()
+        #assert valid values
+        if not self.module.training:
+            if self._is_positive_scale() and (torch.any(value < (self.min_val - self.zero_point) / self.scale - 0.5)):
+                raise AssertionError("Not all elements in the tensor above min val")
+            if self._is_positive_scale() and (torch.any(value > (self.max_val - self.zero_point) / self.scale + 0.5)):
+                raise AssertionError("Not all elements in the tensor below max val")            
+            if self.rnoise_ratio == 0 and not torch.all(value == value.floor()):
+                raise AssertionError("Not all elements in the tensor have integer values.")
+        
+        return value
 
     def dequantize(self, quantized_value):
         """
@@ -91,13 +105,7 @@ class Quantizer:
         return quantized_value + self.zero_point
 
     def _get_qnoise(self, value: Tensor):
-        return torch.clamp(
-            torch.round(value / self.scale - self.zero_point),
-            min=self.min_val,
-            max=self.max_val,
-        ) - value
+        return torch.round(value) - value
 
     def _get_rnoise(self, value: Tensor):
-        return torch.randint(low=-1, high=0, size=value.shape, dtype=value.dtype, device=value.device).add(
-            0.5
-        )
+        return torch.randint(2, size=value.shape, dtype=value.dtype, device=value.device).sub(0.5)
