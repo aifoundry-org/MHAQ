7b7f72f4bcd6127445d1eb87fcd09495db661e5e
Sergey Salishev <sergey.i.salishev@gmail.com>
2024-12-29 15:06:23 +0300

    simplify, fix right bound

--- src/quantization/rniq/layers/rniq_act.py
+++ src/quantization/rniq/layers/rniq_act.py
@@ -19,29 +19,18 @@ class NoisyAct(nn.Module):
         if signed:
             self.act_b = torch.nn.Parameter(self._act_b, requires_grad=True)
         else:
-            self.act_b = torch.nn.Parameter(torch.exp2(self._log_act_q) / 2, requires_grad=False)
+            self.act_b = torch.nn.Parameter(self._act_b, requires_grad=False)
 
         self.log_act_s = torch.nn.Parameter(self._log_act_s, requires_grad=True)
-        self.mode = QMode.ROUND_VAL
-        self.Q = Quantizer(torch.exp2(self._log_act_s), 0, -inf, inf)
+        self.Q = Quantizer(self, torch.exp2(self._log_act_s), 0, -inf, inf)
 
     def forward(self, x):
         s = torch.exp2(self.log_act_s)
         q = torch.exp2(self.log_act_q)
-
-        q_2 = q / 2
-        act_b = self.act_b if self.signed else q_2
-
+        
+        self.Q.min_val = self.act_b
+        self.Q.max_val = self.act_b + q - s
         self.Q.scale = s
-        self.Q.min_val = act_b - q_2
-        self.Q.max_val = act_b + q_2
         self.Q.rnoise_ratio = self._noise_ratio
 
-        # kinda obsolete when we can change noise ratio directly
-        if self.mode == QMode.ROUND_VAL:
-            self.Q.rnoise_ratio = torch.tensor(0)
-        elif self.mode == QMode.SOURCE_VAL:
-            self.Q.rnoise_ratio = torch.tensor(-1)
-        
-        # return self.Q.quantize(x)
         return self.Q.dequantize(self.Q.quantize(x))
