0c51c97b94bd2e543249829aee17391312538d3e
Sergey Salishev <sergey.i.salishev@gmail.com>
2024-12-29 14:50:27 +0300

    convert relu to layer for replacement

--- src/models/resnet/resnet_cifar.py
+++ src/models/resnet/resnet_cifar.py
@@ -71,6 +71,7 @@ class BasicBlock(nn.Module):
             stride=stride, padding=1, bias=False
         )
         self.bn1 = nn.BatchNorm2d(planes)
+        self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(
             planes, planes, kernel_size=3, 
             stride=1, padding=1, bias=False
@@ -78,6 +79,7 @@ class BasicBlock(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
+        self.relu2 = nn.ReLU()
         if stride != 1 or in_planes != planes:
             self.shortcut = LambdaLayer(
                 lambda x: F.pad(x[:, :, ::2, ::2], 
@@ -87,10 +89,10 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu1(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = self.relu2(out)
         return out
 
 
@@ -104,6 +106,7 @@ class ResNet(nn.Module):
             stride=1, padding=1, bias=False
         )
         self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU()
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
@@ -121,7 +124,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
