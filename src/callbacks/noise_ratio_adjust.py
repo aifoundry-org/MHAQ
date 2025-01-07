dfe8e361f323ce6441105aad7ada5ef647a31c60
janchk <janchk@gmail.com>
2025-01-07 17:17:37 +0000

    Config fixes. Changes to noise_ratio callback. Now freq of update is modifiable.

--- src/callbacks/noise_ratio_adjust.py
+++ src/callbacks/noise_ratio_adjust.py
@@ -8,8 +8,9 @@ from lightning.pytorch import Trainer, LightningModule
 logger = logging.getLogger("lightning.pytorch")
 
 class RandNoiseScale(Callback):
-    def __init__(self, reduce_scale=2) -> None:
+    def __init__(self, reduce_scale=2, update_every=10) -> None:
         self.q_loss = 0
+        self.update_every = update_every
         super().__init__()
 
 
@@ -19,6 +20,21 @@ class RandNoiseScale(Callback):
 
     def on_train_batch_end(self, trainer: Trainer, pl_module: LightningModule, outputs, batch, batch_idx: int) -> None:
         self.q_loss += pl_module.wrapped_criterion.wloss.mean().item() + pl_module.wrapped_criterion.aloss.mean().item()
+        if (batch_idx + 1) % self.update_every == 0:
+            noise_ratio = pl_module._noise_ratio
+            if noise_ratio >= 0 and self.q_loss <= 1e-3:
+
+                noise_ratio.data.sub_(0.01)
+                
+                if noise_ratio < 0:
+                    noise_ratio.data.zero_()
+
+                pl_module.noise_ratio(noise_ratio)
+                
+            pl_module.log("RNoise ratio", pl_module._noise_ratio, prog_bar=True)
+
+            self.q_loss = 0
+            
         return super().on_train_batch_end(trainer, pl_module, outputs, batch, batch_idx)
 
 
@@ -27,17 +43,5 @@ class RandNoiseScale(Callback):
 
 
     def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule) -> None:
-        noise_ratio = pl_module._noise_ratio
-        if noise_ratio >= 0 and self.q_loss <= 1e-3:
-
-            noise_ratio.data.sub_(0.01)
-            
-            if noise_ratio < 0:
-                noise_ratio.data.zero_()
-
-            pl_module.noise_ratio(noise_ratio)
-            
-        pl_module.log("RNoise ratio", pl_module._noise_ratio, prog_bar=True)
-
-        self.q_loss = 0
+        
         return super().on_train_epoch_end(trainer, pl_module)
