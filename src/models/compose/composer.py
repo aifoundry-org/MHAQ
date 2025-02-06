da3b3d43650de010604182473ee2718c5324bd29
Sergey Salishev <sergey.i.salishev@gmail.com>
2025-02-06 23:09:08 +0300

    support cpt loading from url
    Former-commit-id: 2fe95cd4150c5788cdc54c6673b52164098a7bf2 [formerly 2fe95cd4150c5788cdc54c6673b52164098a7bf2 [formerly 2fe95cd4150c5788cdc54c6673b52164098a7bf2 [formerly cf541fde1cec2607112e8db7c65713eb1ec1c8ea]]]
    Former-commit-id: 29445205f38b8fbd0b8c0325a4ef728cf8fc83ef
    Former-commit-id: d91ad83f15011bc56b3f4f82fc6c7a09274b6bf7 [formerly 7f17b28fa775d72e62150c901e289c47ba7bfe2d]
    Former-commit-id: 94eb1da5fe484b4625a5aa90a11fc35f35fb2eae

--- src/models/compose/composer.py
+++ src/models/compose/composer.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 import lightning.pytorch as pl
+import torch
 import src.models as compose_models
 
 from torch import nn, optim
@@ -27,6 +29,16 @@ class ModelComposer():
             self.criterion = getattr(nn, training_config.criterion)()
             self.optimizer = getattr(optim, training_config.optimizer)
             self.lr = training_config.learning_rate
+
+            if model_config.cpt_url:
+                state_dict = torch.hub.load_state_dict_from_url(model_config.cpt_url)
+                state_dict = state_dict.get('model', state_dict)   
+                try:             
+                    self.model.load_state_dict(state_dict)
+                except:
+                    wrapper = nn.Sequential(OrderedDict([('module', self.model)]))
+                    wrapper.load_state_dict(state_dict)
+
         else:
             assert(self.model)
             assert(self.model_type)
