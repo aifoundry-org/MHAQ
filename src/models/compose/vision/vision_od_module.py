Correct mAP calculation

src/models/compose/vision/vision_od_module.py
@@ -8,6 +8,10 @@ from src.models.od.loss.yolo_loss import ComputeYoloLoss
 from src.models.od.utils.yolo_nms import non_max_suppression, wh2xy
 from src.models.od.utils.yolo_decode import decode_yolo_nms, compute_metric, compute_ap
 from src.models.od.metrics.map_metrics import MeanAveragePrecisionYolo
+from src.data.compose.vision.od.coco import (
+    transform_coco_outputs,
+    transform_coco_targets,
+)
 
 import torchmetrics.detection
 
@@ -40,9 +44,31 @@ class LVisionOD(pl.LightningModule):
 
         self._init_metrics()
     
-    def on_validation_end(self):
-        # print(np.mean(self._map))
-        return super().on_validation_end()
+    def on_train_start(self):
+        self._batch_size = self.trainer.config.data.batch_size
+        return super().on_train_start()
+
+    def on_validation_start(self):
+        self._batch_size = self.trainer.config.data.batch_size
+        return super().on_validation_start()
+
+    def on_validation_epoch_end(self):
+        map = self.mAP.compute()
+        self.log(
+            f"mAP",
+            map["map"],
+            prog_bar=False,
+            on_epoch=True,
+            batch_size=self._batch_size,
+        )
+        self.log(
+            f"mAP_50",
+            map["map_50"],
+            prog_bar=False,
+            on_epoch=True,
+            batch_size=self._batch_size,
+        )
+        return super().on_validation_epoch_end()
 
     def on_load_checkpoint(self, checkpoint: dict) -> None:
         state_dict = checkpoint["state_dict"]
@@ -80,15 +106,15 @@ class LVisionOD(pl.LightningModule):
         if self.model._get_name() in YOLO_FAMILY:
             target = {k: torch.cat([d[k] for d in target], dim=0) for k in target[0]}
             loss_box, loss_cls, loss_dfl = self.criterion(output, target)
-            self.log("loss_box", loss_box, prog_bar=False)
-            self.log("loss_cls", loss_cls, prog_bar=False)
-            self.log("loss_dfl", loss_dfl, prog_bar=False)
+            self.log("loss_box", loss_box, prog_bar=False, batch_size=self._batch_size)
+            self.log("loss_cls", loss_cls, prog_bar=False, batch_size=self._batch_size)
+            self.log("loss_dfl", loss_dfl, prog_bar=False, batch_size=self._batch_size)
             loss = (
                 loss_box + loss_cls + loss_dfl
             )  # losses are scaled inside criterion using params
         else:
             loss = self.criterion(output, target)
-        self.log("loss", loss, prog_bar=True)
+        self.log("loss", loss, prog_bar=True, batch_size=self._batch_size)
         return loss
 
     def validation_step(self, val_batch, val_index):
@@ -99,35 +125,8 @@ class LVisionOD(pl.LightningModule):
         else:
             output = self.forward(inputs)
             val_loss = self.criterion(output, target)
-        
-        # iou_v = torch.linspace(start=0.5, end=0.95, steps=10)
-        # n_iou = iou_v.numel()
 
-        # outputs = transform_coco_outputs(output)
-        # targets = transform_coco_targets(target)
-        # metrics = []
-        # for i,o in enumerate(outputs):
-        #     m = torch.zeros(o.size(0), n_iou, dtype=torch.bool, device='cuda')
-        #     mask = targets['idx']==i
-        #     c = targets['cls'][mask]
-        #     b = targets['box'][mask]
-        #     if o.size(0)==0:
-        #         if c.numel(): metrics.append((m, *[torch.zeros((2,0))], c.squeeze(-1)))
-        #         continue
-        #     if c.numel():
-        #         m = compute_metric(o[:,:6], torch.cat((c, wh2xy(b)*1),1), iou_v)
-        #     metrics.append((m.cpu(), o[:,4].cpu(), o[:,5].cpu(), c.squeeze(-1).cpu()))
-
-
-
-        # metrics = [torch.cat(x, dim=0).cpu().numpy() for x in zip(*metrics)]
-        map_value = self.mAP(output, target)
-        # tp, fp, m_pre, m_rec, map50, mean_ap = compute_ap(*metrics)
-        # if len(metrics) and metrics[0].any():
-        #     self._map.append(mean_ap)
-        self.log(f"mAP", map_value["map"], prog_bar=True, on_epoch=True, batch_size=5)
-        self.log(f"mAP_50", map_value["map_50"], prog_bar=False, on_epoch=True, batch_size=5)
-
+        self.mAP.update(output, target)
 
     def test_step(self, test_batch, test_index):
         raise NotImplementedError("Test is not yet implemented for OD networks!")

src/quantization/rniq/rniq_quant.py
@@ -112,7 +112,7 @@ class RNIQQuant(BaseQuant):
         for layer in qlayers.keys():
             module = attrgetter(layer)(lmodel.model)
             # if module.kernel_size != (1, 1):
-                # print(layer + " " + repr(module.kernel_size))
+            # print(layer + " " + repr(module.kernel_size))
             preceding_layer_type = layer_types[layer_names.index(layer) - 1]
             if issubclass(
                 preceding_layer_type, (nn.ReLU, nn.SiLU)
@@ -179,6 +179,7 @@ class RNIQQuant(BaseQuant):
     @staticmethod
     def noisy_val_decorator(val_step):
         self = val_step.__self__
+        self._batch_size = self.trainer.config.data.batch_size
 
         def wrapper(*args):
             val_step(*args)
@@ -187,43 +188,49 @@ class RNIQQuant(BaseQuant):
                 "Mean weights bit width",
                 model_stats.get_weights_bit_width_mean(self.model),
                 prog_bar=False,
+                batch_size=self._batch_size,
             )
             self.log(
                 "Actual weights bit width",
                 model_stats.get_true_weights_width(self.model, max=False),
                 prog_bar=False,
+                batch_size=self._batch_size,
             )
             self.log(
                 "Actual weights max bit width",
                 model_stats.get_true_weights_width(self.model),
                 prog_bar=False,
+                batch_size=self._batch_size,
             )
             self.log(
                 "Mean activations bit width",
                 model_stats.get_activations_bit_width_mean(self.model),
                 prog_bar=False,
+                batch_size=self._batch_size,
             )
             self.log(
                 "Actual activations bit widths",
                 model_stats.get_true_activations_width(self.model, max=False),
                 prog_bar=False,
+                batch_size=self._batch_size,
             )
             self.log(
                 "Actual activations max bit widths",
                 model_stats.get_true_activations_width(self.model),
                 prog_bar=False,
+                batch_size=self._batch_size,
             )
 
         return wrapper
-    
+
     @staticmethod
     def noisy_test_decorator(test_step):
         self = test_step.__self__
 
         def wrapper(*args):
             return test_step(*args)
-        return wrapper
 
+        return wrapper
 
     @staticmethod  # yes, it's a static method with self argument
     def noisy_step(self, x):