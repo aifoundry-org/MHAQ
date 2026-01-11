Fix potential gradient error

src/quantization/rniq/calib/minmaxobserver.py
@@ -75,14 +75,15 @@ def apply_quantile_weights_s(module, wbits=8, max_bits = 24, qscheme="per-channe
     for name, m in module.named_modules():
         # TODO: qscheme
         if isinstance(m, (NoisyLinear, NoisyConv2d)):
-            max = m.weight.data.amax((1, 2, 3))
-            min = m.weight.data.amin((1, 2, 3))
+            max_ = m.weight.detach().amax((1, 2, 3))
+            min_ = m.weight.detach().amin((1, 2, 3))
 
             if not m.log_wght_s.requires_grad:
                 wbits = max_bits
 
             # XXX: handle max-min == 0
-            log_s = torch.max(torch.tensor(m.log_wght_s), torch.log2((max - min) / (2**wbits - 1)).reshape(m.log_wght_s.shape))
+            # log_s = torch.max(torch.tensor(m.log_wght_s), torch.log2((max - min) / (2**wbits - 1)).reshape(m.log_wght_s.shape))
+            log_s = torch.max(m.log_wght_s, torch.log2((max_ - min_) / (2**wbits - 1)).reshape(m.log_wght_s.shape))
 
             #m.log_wght_s = torch.nn.Parameter(torch.full(m.log_wght_s.shape, log_s), requires_grad=m.log_wght_s.requires_grad) # per-tensor
             m.log_wght_s = torch.nn.Parameter(log_s, requires_grad=m.log_wght_s.requires_grad)