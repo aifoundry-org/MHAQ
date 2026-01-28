renaming for zero_point and fixes for activations sign choice.

src/quantization/gdnsq/gdnsq_quant.py
@@ -132,11 +132,11 @@ class GDNSQQuant(BaseQuant):
                         qmodel.model, layer, layer_names[layer_names.index(layer) + 1]
                     )
                 if issubclass(
-                    preceding_layer_type, (nn.ReLU, nn.SiLU)
+                    preceding_layer_type, (nn.ReLU)
                 ):  # XXX: hack shoul be changed through config
-                    qmodule = self._quantize_module(module, signed_Activations=False)
+                    qmodule = self._quantize_module(module, signed_activations=False)
                 else:
-                    qmodule = self._quantize_module(module, signed_Activations=False)
+                    qmodule = self._quantize_module(module, signed_activations=True)
 
                 attrsetter(layer)(qmodel.model, qmodule)
 
@@ -480,7 +480,7 @@ class GDNSQQuant(BaseQuant):
             self.qscheme = self.quant_config.qscheme
             self.quant_bias = self.quant_config.quantize_bias
 
-    def _quantize_module(self, module, signed_Activations):
+    def _quantize_module(self, module, signed_activations):
         self.qnmethod = QNMethod[self.quant_config.params.qnmethod]
         if isinstance(module, nn.Conv2d):
             qmodule = self._quantize_module_conv2d(module)
@@ -494,7 +494,7 @@ class GDNSQQuant(BaseQuant):
         if is_biased(module):
             qmodule.bias = module.bias
 
-        qmodule = self._get_quantization_sequence(qmodule, signed_Activations)
+        qmodule = self._get_quantization_sequence(qmodule, signed_activations)
 
         return qmodule
 
@@ -506,7 +506,7 @@ class GDNSQQuant(BaseQuant):
                     (
                         "activations_quantizer",
                         NoisyAct(
-                            signed=True,
+                            signed=signed_activations,
                             disable=disabled,
                         ),
                     ),

src/quantization/gdnsq/layers/gdnsq_act.py
@@ -19,8 +19,8 @@ class NoisyAct(nn.Module):
         super().__init__()
         self.disable = disable
         self.signed = signed
-        zero_point = 0.0 if not signed else -torch.exp2(torch.tensor(init_q - 1).float())
-        self._act_b = torch.tensor([zero_point]).float()
+        biased_point = 0.0 if not signed else -torch.exp2(torch.tensor(init_q - 1).float())
+        self._act_b = torch.tensor([biased_point]).float()
         self._log_act_s = torch.tensor([init_s]).float()
         self._log_act_q = torch.tensor([init_q]).float()
         self._noise_ratio = torch.tensor(noise_ratio)